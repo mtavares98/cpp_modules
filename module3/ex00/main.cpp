cd09346f994239a921727ba0968ba280fb022cda
Miguel Silva <miguel1998282@gmail.com>
2023-03-29 13:05:45 +0100

    Main module3 updated

--- module3/ex00/main.cpp
+++ module3/ex00/main.cpp
@@ -1,10 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 17:37:59 by mtavares          #+#    #+#             */
+/*   Updated: 2023/03/28 17:37:59 by mtavares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <ClapTrap.hpp>
 
 int main(void) {
     ClapTrap bob = ClapTrap("Bob");
     ClapTrap jim = ClapTrap("Jim");
+    ClapTrap bobCpy =ClapTrap(bob);
 
     bob.attack(jim.getName());
     jim.takeDamage(bob.getAttack());
+    bobCpy.attack(jim.getName());
+    jim.takeDamage(bobCpy.getAttack());
     jim.beRepaired(2);
 }
