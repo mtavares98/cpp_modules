c8aff7e25b91d6009db81aced44df94068a9684c
Miguel Silva <miguel1998282@gmail.com>
2023-03-30 16:31:14 +0100

    Finished main testing

--- module3/ex02/main.cpp
+++ module3/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 19:03:43 by mtavares          #+#    #+#             */
-/*   Updated: 2023/03/30 16:18:04 by mtavares         ###   ########.fr       */
+/*   Updated: 2023/03/30 16:30:04 by mtavares         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,12 @@ int main(void) {
     ScavTrap bob = ScavTrap("Bob");
     FragTrap jim = FragTrap("Jim");
 
-    jim.
+    jim.highFivesGuys();
     for (int i = 0; i < 100; i++) {
         if (i % 5 == 0)
             jim.beRepaired(100);
         bob.attack(jim.getName());
         jim.takeDamage(bob.getAttack());
     }
+    bob.attack(jim.getName());
 }
