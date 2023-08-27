db003e93d122f0b3ca56eceb1d35d2cfd3cef9c9
Miguel Silva <mtavares@pop-os.localdomain>
2023-08-27 15:30:15 +0100

    Finished modulo6

---
+++ module6/ex02/main.cpp
@@ -0,0 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtavares <mtavares@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/27 14:09:49 by mtavares          #+#    #+#             */
+/*   Updated: 2023/08/27 15:28:47 by mtavares         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <exception>
+#include <iostream>
+
+#include "A.hpp"
+#include "B.hpp"
+#include "Base.hpp"
+#include "C.hpp"
+
+Base *generate(void) {
+    switch (rand() % 3) {
+        case 0:
+            return new A();
+        case 1:
+            return new B();
+        case 2:
+            return new C();
+    }
+    return NULL;
+}
+
+void identify(Base *p) {
+    if (dynamic_cast<A *>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B *>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C *>(p))
+        std::cout << "C" << std::endl;
+    else
+        std::cout << "Not valid type" << std::endl;
+}
+
+void identify(Base &p) {
+    try {
+        dynamic_cast<A &>(p);
+        std::cout << "A" << std::endl;
+    }
+    catch (std::exception &e) {
+        try {
+            dynamic_cast<B &>(p);
+            std::cout << "B" << std::endl;
+        }
+        catch (std::exception &e) {
+            try {
+                dynamic_cast<C &>(p);
+                std::cout << "C" << std::endl;
+            }
+            catch (std::exception &e) {
+                std::cout << "Not valid type" << std::endl;
+            }
+        }
+    }
+}
+
+int main(void) {
+    Base *b;
+
+    for(int i = 0; i < 100; i++) {
+        b = generate();
+        identify(*b);
+        identify(b);
+        delete b;
+    }
+    Base tmp;
+    identify(tmp);
+    identify(&tmp);
+}
